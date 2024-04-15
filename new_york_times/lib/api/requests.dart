24474146f9623e6a13c1c93916ecdf1b91745dbf
Arseny Arnold <kulbakocnt@gmail.com>
2024-04-15 22:22:40 +0300

    added api and start working with him

--- new_york_times/lib/api/requests.dart
+++ new_york_times/lib/api/requests.dart
@@ -2,11 +2,23 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future<void> fetchNYTData() async {
-  final response = await http.get(Uri.parse('https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=BrSKiRDPz9a6GVtkFZTrFHAg7ChR3U0F'));
+  final response = await http.get(Uri.parse('https://api.nytimes.com/svc/news/v3/content/all/all.json?api-key=3XdEX34GAphIu6ABBP08cCtOh8DBxubC'));
 
   if (response.statusCode == 200) {
     var jsonResponse = jsonDecode(response.body);
-    print(jsonResponse);
+    List<dynamic> titles = [];
+    List<dynamic> abstracts = [];
+    List<dynamic> images = [];
+
+    for (var result in jsonResponse['results'].take(4)) {
+      titles.add(result['title']);
+      abstracts.add(result['abstract']);
+      images.add(result['multimedia'][0]['url']);
+    }
+
+    print('Titles: $titles');
+    print('Abstracts: $abstracts');
+    print('Images: $images');
   } else {
     print('Request failed with status: ${response.statusCode}.');
   }
